4ba4a4e5d757ac576fde1850c13ada15ab6a1c76
Andrey Semerikov <fakt37@gmail.com>
2022-07-25 21:19:16 +0300

    add MarvelService

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './components/app/App';
+import MarvelService from './service/MarvelService';
+
+import './style/style.scss';
+
+const marvelService = new MarvelService();
+
+marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
---
+++ src/service/MarvelService.js
@@ -0,0 +1,26 @@
+
+
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=2aeb76950da89e5112db0096de183612';
+
+    getResource = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+    }
+
+    getAllCharacters = () => {
+        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    }
+
+    getCharacter = (id) => {
+        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    }
+}
+
+export default MarvelService;
